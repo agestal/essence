495d27999a94ee1091ac39398ad2ed3be7be01e5
agestal <adrian88gm@gmail.com>
2021-04-03 12:13:47 +0200

    Cambio de diseño

---
+++ resources/views/top.blade.php
@@ -0,0 +1,40 @@
+<body  data-home-page-title="Essence Redondela" class="u-body">
+  <header style="background-color: #000000; color: #a4ab3d;" class="u-clearfix u-header" id="sec-771b">
+    <div class="u-align-left u-clearfix u-sheet u-sheet-1">
+        <h3 class="u-align-left u-custom-font u-headline u-text u-text-font u-text-1">
+          <a href="/">Essence Redondela</a>
+        </h3>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1" data-responsive-from="XL">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#" style="padding: 8px 0px;">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                version="1.1" id="svg-a760" x="0px" y="0px" 
+                viewBox="0 0 302 302" 
+                style="enable-background:new 0 0 302 302;" 
+                xml:space="preserve" 
+                class="u-svg-content">
+                <g>
+                <rect y="36" width="302" height="30"></rect>
+                <rect y="236" width="302" height="30"></rect>
+                <rect y="136" width="302" height="30"></rect>
+                </g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul  class="u-nav u-spacing-25 u-unstyled">
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-2.html" style="padding: 8px 0px;">Page 2</a></li>
+            </ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-2.html" style="padding: 8px 0px;">Page 2</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div>
+</header> 
