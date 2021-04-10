044173d04c2fdf31485e696f43e239b7af176988
agestal <adrian88gm@gmail.com>
2021-04-10 09:43:29 +0200

    CORS

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('pedirvez','VecesController@pedirvez');
+Route::group(['middleware' => ['cors']], function () {
+    Route::post('pedirvez','VecesController@pedirvez');
+});
