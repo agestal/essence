33e91633fc30362dc5a1bf73cc702808fc78fe6a
agestal <adrian88gm@gmail.com>
2021-04-10 10:02:48 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['middleware' => ['cors']], function () {
-    Route::post('pedirvez','VecesController@pedirvez');
+    Route::post('pedirvez','VecesController@pedirvezapi');
 });
