27ca51f31ca311cac59868cdcb9d4a9a01c98d86
Adrian <agestal@arnoia.com>
2022-09-02 09:41:39 +0200

    mix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html style="font-size: 16px;">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="page_type" content="np-template-header-footer-from-plugin">
+ 
+   
     <title>Essence Salones Low Cost - Redondela </title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
-<link rel="stylesheet" href="Page-2.css" media="screen">
+    <link rel="stylesheet" href="Page-2.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Dancing+Script:400,500,600,700">
-    <meta property="og:title" content="Page 2">
-    <meta property="og:type" content="website">
-    <meta name="theme-color" content="#fbd2c0">
+    @laravelPWA
   </head>
