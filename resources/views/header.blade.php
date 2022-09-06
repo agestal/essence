e7cc53d2bffc0591da8f8af5e433c783e97a58c3
Adrian <agestal@arnoia.com>
2022-09-06 08:47:38 +0200

    PWA

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,5 +9,36 @@
     <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Dancing+Script:400,500,600,700">
-    @PWA
+    @laravelPWA
+    <script>
+  window.onload = (e) => { 
+    // Declare init HTML elements
+    const buttonAdd = document.querySelector('#buttonAdd');
+
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+    });
+
+    // Add event click function for Add button
+    buttonAdd.addEventListener('click', (e) => {
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+  }
+    </script>
   </head>
+  <input type="button" id="buttonAdd" />
